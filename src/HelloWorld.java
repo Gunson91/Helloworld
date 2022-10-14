a50621126f9455304c635ee7c729fddced6085d0
Gunson91 <martin.91.gun@gmail.com>
2022-10-14 12:53:08 +0200

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Type in the Hello World program; then compile and run it
+        System.out.println("Hello World!");
+        // Add a print statement that displays a second message
+        System.out.println("Are you ready??");
+
+    }
+
+}
